789539421922111a014ef5756302c7b3f93aedad
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-09-05 20:13:51 +0530

    initializing passport

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
+const express_sessions = require('express-session');
+const passport = require('./passport').passport;
 
 //setting up enviornment
 dotenv.config({path: "./config/config.env"});
@@ -11,7 +13,16 @@ require('./config/connection_db').connect_with_MongoDB();
 const app = express();
 let port = process.env.PORT || 3000;
 
+//decrypting json and urlencoded
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
+//initializing passport
+app.use(express_sessions({
+    secret: 'SachinTendulkarIsTheGreatestBatsmanOfAllTime'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 app.listen(port, ()=>{console.log(`Mode: ${process.env.NodeEnv} hosted on port ${port}`)});
