878c87feb1e3ff2fdb399a409b9516755c0110a6
Mayukh Chakrabarti <mayukhchakrabarti99@gmail.com>
2020-09-05 10:02:06 +0530

    Configuring process env and connecting to cloud cluster

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+
+//setting up enviornment
+dotenv.config({path: "./config/config.env"});
+
+//connecting to database
+require('./config/connection_db').connect_with_MongoDB();
+
+const app = express();
+let port = process.env.PORT || 3000;
+
+
+app.listen(port, ()=>{console.log(`Mode: ${process.env.NodeEnv} hosted on port ${port}`)});
+
